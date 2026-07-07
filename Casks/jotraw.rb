3e01d38e767640333225681c229946fae69c08f3
Tommi Tarkiainen <tommi.tarkiainen@gmail.com>
2026-07-07 08:49:47 +0300

    Release 1.0.15: bump cask

--- Casks/jotraw.rb
+++ Casks/jotraw.rb
@@ -1,6 +1,6 @@
 cask "jotraw" do
-  version "1.0.14"
-  sha256  "39156cbb51be249efc2b3038a49e3a1934ddf26c5a2e0e2dddcbe6a2000f9242"
+  version "1.0.15"
+  sha256  "44e05e8b698d483154b7046c2599b2f19e5bba51bfe7575c8d0c9a0864a438ba"
 
   # TODO: replace with the real release URL once published.
   # The artifact should be a notarized, code-signed zip containing JotSync.app
